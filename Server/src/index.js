30f2a1bb6471981e8a167979a3d995f03f0c1deb
GOVARDAN-N-A <e1215024govardancse@gmail.com>
2025-01-13 10:03:04 +0530

    Working on deployment - IV

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,22 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { fileURLToPath } from "url"; // for ES module compatibility
+
 import { connectDB } from "./lib/db.lib.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.lib.js";
-
-// Resolve __dirname for ES modules
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
 
-app.use(express.json({ limit: '50mb' }));
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -29,8 +31,17 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Express's built-in body parser for URL-encoded data
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+app.use(bodyParser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+  }));
+
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
